326791dbcb8d206db0cee0ddb11dc5f5626e5087
AndrewKralovec <akrala@siu.edu>
2016-03-21 09:34:01 -0500

    update -Andrew Kralovec

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/:username', function (req, res, next) {
     var username = req.params.username;
     var db = new Db('AlphaLearning', new Server('localhost', 27017));
             db.open(function (err, db) {
-                // Get user object 
+                // Get user object, This features will be removed once the sessions feature is added
                 db.collection('Accounts').findOne({username:username}, function (err, doc) {
                     assert.equal(null, err);
                     if (doc != null) {
