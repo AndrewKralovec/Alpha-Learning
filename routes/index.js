8a2cf99b4094906a498c1ee995c788b29b95c8af
AndrewKralovec <akrala@siu.edu>
2016-04-02 19:48:11 -0500

    Fix error route jade

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ router.get('/:username', function (req, res, next) {
                         res.render('home', { title: 'User Hame Page', message: username, documents:documents});
                     }
                     else {
-                        res.render('404', { title: 'Error Page'});
+                        res.render('error', { title: 'Error Page'});
                     }
                 });
             });
@@ -34,7 +34,22 @@ router.get('/:username', function (req, res, next) {
 
 // Test out user profile pages 
 router.get('/:username/Profile',function(req,res,next){
-  res.render('profile',{title:"Profile "}); 
+    var username = req.params.username;
+    var db = new Db('AlphaLearning', new Server('localhost', 27017));
+            db.open(function (err, db) {
+                // Get user object, This features will be removed once the sessions feature is added
+                db.collection('Accounts').findOne({username:username}, function (err, doc) {
+                    assert.equal(null, err);
+                    if (doc != null) {
+                        var documents = doc.Courses ; 
+                        console.log("Found");
+                        res.render('profile', { title: 'User Hame Page', message: username, documents:documents});
+                    }
+                    else {
+                        res.render('error', { title: 'Error Page'});
+                    }
+                });
+            });
 });
 // Test out quiz pages
 router.get('/:username/Quiz',function(req,res,next){
@@ -42,7 +57,7 @@ router.get('/:username/Quiz',function(req,res,next){
 });
 // ERROR
 router.get('/error', function (req, res, next) {
-    res.render('404', { title: 'Error Page'});
+    res.render('error', { title: 'Error Page'});
 });
 
 
