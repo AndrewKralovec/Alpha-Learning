46b07b17af0bcc6f4eb8344f525fdcaae170fc4f
AndrewKralovec <akrala@siu.edu>
2016-04-07 15:10:09 -0500

    editing controller

--- routes/upload.js
+++ routes/upload.js
@@ -31,5 +31,38 @@ router.get('/error', function (req, res, next) {
     res.render('404.jade', { title: 'Error Page'});
 });
 
+// Listen for Upload file 
+router.post('/uploadFile', function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            console.log("Error uploading file");
+        } else {
+            //var databaseName = "E-learn", collection = "Accounts";
+            var username = req.body.username ; 
+            var filename = req.file.originalname ; 
+            var savename = req.file.filename ; 
+            var filePath = req.file.path ; 
+            var filetype = req.file.mimetype ; 
+            console.log("GET username " + username);
+            console.log("GET fieldname " + req.file.fieldname);
+            console.log("GET filename " + filename);
+            console.log("GET orignal name " + savename);
+            console.log("GET path " + filePath);
+            console.log("GET filetype " + filetype);
+            var db = new Db('AlphaLearning', new Server('localhost', 27017));
+            db.open(function (err, db) {
+                // Get user object 
+                db.collection('Accounts').findOne({username:username}, function (err, doc) {
+                    assert.equal(null, err);
+                    if (doc != null) {
+                        console.log("Found");
+                        db.close(); 
+                    } 
+                });
+            });
+        }
+    });
+});
+
 
 module.exports = router;
