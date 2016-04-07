8144e61812066e51c837d87e697303e7f581cecc
AndrewKralovec <akrala@siu.edu>
2016-04-07 14:57:32 -0500

    multer config -Andrew Kralovec

--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './uploads');
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now());
+    }
+});
+var upload = multer({ storage: storage }).single('file');
+// All possible mongo db objects 
+var Db = require('mongodb').Db,
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    ReplSetServers = require('mongodb').ReplSetServers,
+    ObjectID = require('mongodb').ObjectID,
+    Binary = require('mongodb').Binary,
+    GridStore = require('mongodb').GridStore,
+    Grid = require('mongodb').Grid,
+    Code = require('mongodb').Code,
+    assert = require('assert');
 
 // Will add :username
 router.get('/', function (req, res, next) {
