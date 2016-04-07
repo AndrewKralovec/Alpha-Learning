db20ea1637631a64935df242f676d700daeaa08f
AndrewKralovec <akrala@siu.edu>
2016-04-07 15:15:10 -0500

    Edited upload page, with new styling

--- public/javascripts/uploadCtrl.js
+++ public/javascripts/uploadCtrl.js
@@ -7,20 +7,21 @@
 //inject angular file upload directives and services.
 var app = angular.module('fileUpload', ['ngFileUpload']);
 
-app.controller('MyCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
+app.controller('uploadController', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
     $scope.uploadPic = function(file) {
     file.upload = Upload.upload({
-      url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-      data: {username: $scope.username, file: file},
+      url: 'fileUpload/uploadFile',
+      data: {file: file, username: $scope.username}, // extensions are not need because linux is GOD 
     });
 
     file.upload.then(function (response) {
+      var duck = response ; // debug response 
       $timeout(function () {
         file.result = response.data;
       });
     }, function (response) {
-      //if (response.status > 0)
-       // $scope.errorMsg = response.status + ': ' + response.data;
+      if (response.status > 0)
+        $scope.errorMsg = response.status + ': ' + response.data;
     }, function (evt) {
       // Math.min is to fix IE which reports 200% sometimes
       file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
