079ddaeb02e8562676d602a35e6a8b55ebd2df72
AndrewKralovec <akrala@siu.edu>
2016-04-10 13:54:31 -0500

    Created a favicon/icon for the site

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// Configuring Passport
+var passport = require('passport');
+var expressSession = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+//var flash = require('connect-flash');
 
 var routes = require('./routes/index');
 var login = require('./routes/login');
@@ -13,17 +19,35 @@ var upload = require('./routes/upload');
 var post = require('./routes/post');
 var app = express();
 
+var User = ({
+        username: String,
+    password: String,
+    email: String,
+    gender: String,
+    address: String
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public','images','favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Configuring Passport
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+ // Using the flash middleware provided by connect-flash to store messages in session
+ // and displaying in templates
+//var flash = require('connect-flash');
+//app.use(flash());
 
 // Have root route to the login page.
 // Once sessions are in place, the routes will resemble a average websites. 
