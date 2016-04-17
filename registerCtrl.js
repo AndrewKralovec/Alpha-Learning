b2a219ead222c6af8a988390947db571b0d21a77
Tim Lunik <timlunik@siu.edu>
2016-04-16 20:15:38 -0500

    registration files

---
+++ registerCtrl.js
@@ -0,0 +1,14 @@
+var app = angular.module('myApp',[]);
+app.controller('registerController',function($scope,$http) {
+    $scope.register = function(username,password) {
+        var objectJson = {'username':username,'password':password};
+        // Send user login object to server for validation
+        $http.post('/register',objectJson).success(function(response) {
+            // Redirect to the correction location
+            location.href = response.address;
+        }).error(function(response){
+            // We did not find the user in the database
+            alert("Failed registration");
+        });
+    };
+});
