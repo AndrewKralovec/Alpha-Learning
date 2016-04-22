8351ca67959dae385cf9236f85251852f9839b9d
AndrewKralovec <akrala@siu.edu>
2016-04-21 23:06:40 -0500

    Changing Course object

--- public/javascripts/courseCtrl.js
+++ public/javascripts/courseCtrl.js
@@ -3,17 +3,23 @@
  * Logic written in Javascript, responsible for formatting what the user sees on the Course page
  * This, for the moment, includes the count of courses,and at which index to split the courses up on the page 
  */
-var app = angular.module('courseModule', [],function($locationProvider){
+var app = angular.module('courseModule', [], function ($locationProvider) {
     // Rewrite $locationProvider, to use hashbang as default mode.
     // ALL link tags must have the '_self', so it puts the new document in the same window and frame as the current document.
     $locationProvider.html5Mode(true);
 });
 app.controller('courseCtrl', function ($scope, $http, $location) {
     $scope.Math = window.Math;
-    $scope.currentUrl = $location.path(); 
+    $scope.currentUrl = $location.path();
+    $scope.postRequest = function (address,post) {
+        // Save speed, by posting arleady loaded post object to router 
+        $http.post(address, post).success(function (response) {
+            // All good
+        });
+    };
     $scope.getPags = function (num) {
-        alert(Math.ceil(num/1));
-        return (new Array(Math.ceil(num/1))); 
+        alert(Math.ceil(num / 1));
+        return (new Array(Math.ceil(num / 1)));
     }
     $scope.range = function (min, max) {
         return function (doc) {
@@ -22,11 +28,11 @@ app.controller('courseCtrl', function ($scope, $http, $location) {
         };
     };
     //$scope.documents = documents ; 
-    
+
     // Show corresponding content to the file type. 
     $scope.showType = function (type) {
         if (type.includes("image")) {
-            return true; 
+            return true;
             // }else if(type.includes("text")){
             // }else if(type.includes("pdf")){
         }
--- public/javascripts/loginCtrl.js
+++ public/javascripts/loginCtrl.js
@@ -2,17 +2,17 @@
  * Login controller 
  * Logic written in Javascript, responsible for checking user login creditionals, and then logging the user in  
  */
-var app = angular.module('myApp',[]); 
-app.controller('loginController',function($scope,$http) {
-		$scope.login = function(username,password) {
-		var objectJson = {'username':username,'password':password}; 
-    // Send user login object to server for validation 
-		$http.post('/loginRequest',objectJson).success(function(response) {
-            // Redirect to the correction location 
-            location.href = response.address;
-      }).error(function(response){
-          // We did not find the user in the database 
-          alert("Failed login");
-      });
-	};
+var app = angular.module('myApp', []);
+app.controller('loginController', function ($scope, $http) {
+      $scope.login = function (username, password) {
+            var objectJson = { 'username': username, 'password': password };
+            // Send user login object to server for validation 
+            $http.post('/loginRequest', objectJson).success(function (response) {
+                  // Redirect to the correction location 
+                  location.href = response.address;
+            }).error(function (response) {
+                  // We did not find the user in the database 
+                  alert("Failed login");
+            });
+      };
 }); 
